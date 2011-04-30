0d11f52954b862fe7e889b998fedfa01c7276ba2
Pawel Majewski <majer@op.pl>
2011-04-30 20:53:47 +0200

    wygenerowany rdzeń aplikacji

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fortunka::Application.config.secret_token = '9a1921b13fb290904ac23746e69078947697ea6ba50588db8786f9e1804881e18b7e399b6e448de06b21e040ef1fe3fb64a1dea7dfc4489f4d3adbb6006e22b2'
